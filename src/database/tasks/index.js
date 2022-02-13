26425a8aaff3805b251a8305e6f169b3920bebe8
Elias Forte <eliasforte.dev@gmail.com>
2022-02-13 03:16:08 -0300

    fix: indentation into task model

--- src/database/tasks/index.js
+++ src/database/tasks/index.js
@@ -7,7 +7,5 @@ const DB_COLLECTION = 'tasks';
 const userCollection = client.db(DB_NAME).collection(DB_COLLECTION);
 
 module.exports.Create = async ({ text, userId, status, createAt }) => await userCollection.insertOne({ text, userId, status, createAt });
-
 module.exports.ListTask = async (userId) => await userCollection.find(userId).toArray();
-
 module.exports.RemoveTask = async (id) => await userCollection.deleteOne({ _id: ObjectId(id) });
