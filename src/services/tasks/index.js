8cecc05ee1986c8b93ab89c285ea274452b78b0c
Elias Forte <eliasforte.dev@gmail.com>
2022-02-13 03:13:55 -0300

    fix: service function CreateTask

---
+++ src/services/tasks/index.js
@@ -0,0 +1,12 @@
+const { Create, ListTask, RemoveTask, FindByName } = require('../../database/tasks');
+const { ApiError: { NewError } } = require('../../global/error/apiError');
+const { INVALID_ID_400 } = require('../../global/helpers/messages');
+const { ObjectId } = require('mongodb');
+
+const validadeId = (id) => {
+  if (!ObjectId.isValid(id)) return NewError(INVALID_ID_400);
+};
+
+module.exports.CreateTask = async ({ text, userId, status, createAt }) => {
+  return await Create({ text, status, createAt, userId });
+};
