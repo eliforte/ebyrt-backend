9bbbc7fc168040a7e33f4ad7e1a66d071d66a99d
Elias Forte <eliasforte.dev@gmail.com>
2022-02-13 03:27:20 -0300

    created: UpdateTask service

--- src/database/tasks/index.js
+++ src/database/tasks/index.js
@@ -9,3 +9,11 @@ const userCollection = client.db(DB_NAME).collection(DB_COLLECTION);
 module.exports.Create = async ({ text, userId, status, createAt }) => await userCollection.insertOne({ text, userId, status, createAt });
 module.exports.ListTask = async (userId) => await userCollection.find(userId).toArray();
 module.exports.RemoveTask = async (id) => await userCollection.deleteOne({ _id: ObjectId(id) });
+module.exports.UpdateTask = async (id, infoTask) => {
+  const { value } = await userCollection.findOneAndUpdate(
+    { _id: ObjectId(id) },
+    { $set: infoTask },
+  );
+
+  return value;
+}
