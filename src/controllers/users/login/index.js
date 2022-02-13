3cb2ba2adc1c24ea2a277de298c92193f396783d
Elias Forte <eliasforte.dev@gmail.com>
2022-02-13 02:46:31 -0300

    fix: export service function LoginUser

--- src/controllers/users/login/index.js
+++ src/controllers/users/login/index.js
@@ -1,10 +1,10 @@
 const { ACCEPTED } = require('http-status-codes').StatusCodes;
-const service = require('../../../services/users');
+const { LoginUser } = require('../../../services/users');
 
 module.exports.Login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    const loginAccepted = await service.loginUser({ email, password });
+    const loginAccepted = await LoginUser({ email, password });
     return res.status(ACCEPTED).json({ token: loginAccepted });
   } catch (err) {
     next(err)
