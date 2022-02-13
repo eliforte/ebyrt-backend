7cf63279dd2ab4fa113fb623f558621f78015899
Elias Forte <eliasforte.dev@gmail.com>
2022-02-13 01:38:51 -0300

    created: route task into root controller

--- src/controllers/root.js
+++ src/controllers/root.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routerLogin = require('./users/login/route');
+const routerTask = require('./tasks/route');
 const validation = require('../global/middlewares/validation');
 
 const root = express.Router({ mergeParams: true });
 
 root.use('/login', validation.Login, routerLogin);
+root.use('/task', routerTask)
 
 module.exports = root;
