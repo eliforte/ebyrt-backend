873327607ed60e55e1f59f80fd8279ef0303223c
Elias Forte <eliasforte.dev@gmail.com>
2022-02-13 05:21:23 -0300

    fix: body response

--- src/controllers/users/register/index.js
+++ src/controllers/users/register/index.js
@@ -5,7 +5,7 @@ module.exports.Register = async (req, res, next) => {
   try {
     const { email, password, name } = req.body;
     const newUser = await CreateUser({ email, password, name });
-    return res.status(CREATED).json({ message: 'Successfully created user!', token: newUser });
+    return res.status(CREATED).json(newUser);
   } catch (err) {
     next(err);
   }
