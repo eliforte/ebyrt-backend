0c1e28ce13ed68a151832b36c1e831ffa99234d4
Elias Forte <eliasforte.dev@gmail.com>
2022-02-13 01:24:51 -0300

    created: controller task delete

--- src/controllers/tasks/index.js
+++ src/controllers/tasks/index.js
@@ -1,4 +1,4 @@
-const { ACCEPTED, OK } = require('http-status-codes').StatusCodes;
+const { ACCEPTED, OK, NO_CONTENT } = require('http-status-codes').StatusCodes;
 const service = require('../../services/tasks');
 
 module.exports.Create = async (req, res, next) => {
@@ -16,6 +16,16 @@ module.exports.List = async (req, res, next) => {
   try {
     const list = await service.listTasks();
     return res.status(OK).json(list);
+  } catch (err) {
+    next(err)
+  }
+};
+
+module.exports.Delete = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await service.removeTask(id);
+    return res.status(NO_CONTENT).end();
   } catch (err) {
     next(err)
   }
