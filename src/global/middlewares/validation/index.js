e2930163f993b972bbaf3aa74c86a7ae23668a53
Elias Forte <eliasforte.dev@gmail.com>
2022-02-13 00:26:47 -0300

    created: middleware validation tasks

--- src/global/middlewares/validation/index.js
+++ src/global/middlewares/validation/index.js
@@ -2,6 +2,7 @@ const { BAD_REQUEST } = require('http-status-codes').StatusCodes;
 const {
   SCHEMALogin,
   SCHEMARegister,
+  SCHEMATask,
 } = require('../../helpers/schemas');
 
 module.exports.Login = async (req, _res, next) => {
@@ -17,6 +18,17 @@ module.exports.Login = async (req, _res, next) => {
 module.exports.Register = async (req, _res, next) => {
   try {
     const { error } = SCHEMARegister.validate(req.body);
+    if (error) return next({ message: error.message, status: BAD_REQUEST });
+    next();
+  } catch (err) {
+    next(err)
+  }
+};
+
+module.exports.Tasks = async (req, _res, next) => {
+  try {
+    const { text, userId, status } = req.body;
+    const { error } = SCHEMATask.validate({ text, userId, status });
     if (error) return next({ message: error.message, status: BAD_REQUEST });
     next();
   } catch (err) {
