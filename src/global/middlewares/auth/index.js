6223e153c3bdefd8928acd69c8ec8b2d9360a68f
Elias Forte <eliasforte.dev@gmail.com>
2022-02-13 04:44:10 -0300

    fix: middleware auth import

--- src/global/middlewares/auth/index.js
+++ src/global/middlewares/auth/index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const { UNAUTHORIZED } = require('http-status-codes').StatusCodes;
-const messages = require('../../helpers/messages');
-const modelUsers = require('../../../database/users');
+const { JWT_MALFORMED_401, MISSING_TOKEN_401 } = require('../../helpers/messages');
+const { FindByEmail } = require('../../../database/users');
 
 const { SECRET } = process.env;
 const jwtConfig = {
@@ -16,15 +16,15 @@ module.exports.VerifyToken = async (req, res, next) => {
   try {
     const { authorization } = req.headers;
     if (!authorization) {
-      return res.status(UNAUTHORIZED).json(messages.MISSING_TOKEN_401);
+      return res.status(UNAUTHORIZED).json(MISSING_TOKEN_401);
     }
     const decoded = jwt.verify(authorization, process.env.SECRET);
     const { email } = decoded.data;
-    const foundedEmail = await modelUsers.findByEmail(email);
-    if (!foundedEmail) return next(messages.JWT_MALFORMED_401);
+    const foundedEmail = await FindByEmail(email);
+    if (!foundedEmail) return next(JWT_MALFORMED_401);
     req.user = decoded.data;
     next();
   } catch (err) {
-    next(messages.JWT_MALFORMED_401);
+    next(JWT_MALFORMED_401);
   }
 }
